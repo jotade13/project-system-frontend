a86c9f4407655f531a1cb8f3dfc942b9e101489c
jotade13 <jdmds13@gmail.com>
2025-04-09 21:55:26 -0400

    added Button

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,10 +2,10 @@ import { Button } from "./ui/button";
 const Header = () => {
     return (
     <header className="flex w-full">
-        <p className="text-4xl">
+        <p className="text-4xl w-full">
             Sistema de Gestion de proyectos y tareas
         </p>
-        <Button className="self">Salir</Button>
+        <Button className="mr-2 my-1">Salir</Button>
     </header>
     )
 }
