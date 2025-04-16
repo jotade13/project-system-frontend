6bb3fa22e0c7050e40ffc0f919f32f0d963971ac
Jotade13 <jdmds13@gmail.com>
2025-04-16 17:14:30 -0400

    installed i next18

--- src/main.tsx
+++ src/main.tsx
@@ -7,6 +7,8 @@ import { ThemeProvider } from "@/components/theme-provider"
 import { QueryClientProvider } from '@tanstack/react-query';
 import { queryClient } from './util/http';
 
+import i18n from './util/i18n';
+
 
 
 createRoot(document.getElementById('root')!).render(
